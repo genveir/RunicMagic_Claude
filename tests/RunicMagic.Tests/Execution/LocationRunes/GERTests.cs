6db41576a1382de9fba65d550c6cf4c52ece6c93
Geerten Vink <maedaros@yahoo.com>
2026-04-29 13:41:32 +0200

    Updated location runes to fall back to executor location

--- tests/RunicMagic.Tests/Execution/LocationRunes/GERTests.cs
+++ tests/RunicMagic.Tests/Execution/LocationRunes/GERTests.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using RunicMagic.Tests.Builders;
+using RunicMagic.World.Execution;
 using RunicMagic.World.Geometry;
 using RunicMagic.World.Runes.LocationRunes;
 using Xunit;
@@ -61,13 +62,14 @@ public class GERTests
     }
 
     [Fact]
-    public void Evaluate_EmptySet_ReturnsOrigin()
+    public void Evaluate_EmptySet_ReturnsExecutorLocation()
     {
+        var executorEntity = new EntityBuilder().WithLocation(x: 200, y: 400).WithWeight(10).Build();
         var ger = new GER(new FixedEntitySet());
-        var context = TestFixtures.MakeContext();
+        var context = TestFixtures.MakeContext(executor: new EntitySet([executorEntity]));
 
         var result = ger.Evaluate(context);
 
-        result.Should().Be(new Location(0, 0));
+        result.Should().Be(new Location(200, 400));
     }
 }
